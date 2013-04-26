ea6403c36d330f96ce4456c21094559ea5377976
Zarek <zarek.p.jenkinson@gmail.com>
2013-04-27 00:44:54 +1200

    woo, open graph

--- modules/open_graph.py
+++ modules/open_graph.py
@@ -2,12 +2,22 @@
 # vim: noai:ts=4:sw=4:expandtab:syntax=python
 
 import re
+import urllib
+import lxml.html
 from xbotpp.modules import Module
 
 class open_graph(Module):
     """\
     Open Graph URL scanning module.
     """
+
+    class HeadRequest(urllib.request.Request):
+        """\
+        A :py:class:`urllib.request.Request` object to represent a HTTP HEAD request.
+        """
+
+        def get_method(self):
+            return "HEAD"
 
     def __init__(self):
         self.bind = [
@@ -35,7 +45,9 @@ class open_graph(Module):
                     continue
 
             try:
-                self.bot.connection.privmsg(event.target, self.do_og(url[0]))
+                result = self.do_og(url[0])
+                if result:
+                    self.bot.connection.privmsg(event.target, result)
             except:
                 raise
 
@@ -55,4 +67,44 @@ class open_graph(Module):
             if re.search(module, url):
                 self.bot._debug("Found matching URL module, calling")
                 return self.bot.modules.modules['url'][module][0](self.bot, None, url, "")
+
+        # Get the headers of the URL
+        self.bot._debug("Sending HEAD request...")
+        try:
+            response = urllib.request.urlopen(self.HeadRequest(url), timeout=2)
+        except urllib.request.URLError:
+            return None
+
+        # Check if we have an HTML document
+        conttype = response.info().get_content_type()
+        self.bot._debug("Content-Type: %s" % conttype)
+        if not re.search("html", conttype):
+            bot._debug("Abort Open Graph scan.")
+            return None
+        
+        self.bot._debug("Fetching OpenGraph data...")
+
+        og = {}
+        html = lxml.html.document_fromstring(urllib.request.urlopen(url, timeout = 5).read())
+        for element in html.xpath('//meta'):
+            prop = element.get('property')
+            if prop and prop.startswith('og:'):
+                og[prop[3:]] = element.get('content')
+
+        if og and 'title' in og:
+            self.bot._debug("Found Open Graph data.")
+
+            if 'site_name' in og:
+                return "%s: \x02%s\x02" % (og['site_name'], og['title'])
+            else:
+                return "\x02%s\x02" % og['title']
+        else:
+            try:
+                self.bot._debug("Falling back to <title> element.")
+                title = html.xpath("//title/text()")[0]
+                title = str(re.sub("[^a-zA-Z0-9\\.\-\|\s\\\\\\/!@#\$%|^&*(){}\[\]_+=<>,\?'\":;\~\`]", '', title), 'utf-8')
+                title = " ".join([s.strip() for s in title.split()])
+                return "\x02%s\x02" % title
+            except:
+                return None
 
