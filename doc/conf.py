f0e3e1c5d628fa1e28b044c507aeedca735b8c62
Zarek Jenkinson <zarek.p.jenkinson@gmail.com>
2013-05-06 04:31:38 +1200

    yeah i really should test this bastard before pushing code huh

--- doc/conf.py
+++ doc/conf.py
@@ -74,7 +74,7 @@ with open("modules/index.rst", "w") as moduleindex:
                 
                 bind = { "command": [], "url": [], "privmsg": [] }
                 for line in member[1]().bind:
-                    bind[command.type].append("%s (`%s`)" % (line.name, line.func.__name__))
+                    bind[line.type].append("%s (`%s`)" % (line.name, line.func.__name__))
 
                 command = ", ".join(bind['command'])
                 url = ", ".join(bind['url'])
