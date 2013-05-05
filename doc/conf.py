3e15cb4df53eea4ea6cf0660df1e228a13310c03
Zarek Jenkinson <zarek.p.jenkinson@gmail.com>
2013-05-06 04:22:53 +1200

    lol that's why the docs don't build

--- doc/conf.py
+++ doc/conf.py
@@ -74,7 +74,7 @@ with open("modules/index.rst", "w") as moduleindex:
                 
                 bind = { "command": [], "url": [], "privmsg": [] }
                 for line in member[1]().bind:
-                    bind[line[0]].append("%s (`%s`)" % (line[1], line[2].__name__))
+                    bind[line[0]].append("%s (`%s`)" % (line.name, line.func.__name__))
 
                 command = ", ".join(bind['command'])
                 url = ", ".join(bind['url'])
