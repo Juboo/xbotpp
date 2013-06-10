85640ba5a513987fdcbdfd034ce0351840f6aa67
Zarek Jenkinson <aki@aki.pw>
2013-06-10 23:35:08 +1200

    spaces

--- xbotpp/handler/handlers.py
+++ xbotpp/handler/handlers.py
@@ -4,24 +4,24 @@ from xbotpp import debug
 from xbotpp.handler import event
 
 dispatch = {
-	'message': [],
-	'user_join': [],
-	'user_part': [],
-	'user_change_nick': [],
+    'message': [],
+    'user_join': [],
+    'user_part': [],
+    'user_change_nick': [],
 }
 
 def on_message(revent):
     for function in dispatch['message']:
-    	function(revent)
+        function(revent)
 
 def on_user_join(revent):
     for function in dispatch['user_join']:
-    	function(revent)
+        function(revent)
 
 def on_user_part(revent):
     for function in dispatch['user_part']:
-    	function(revent)
+        function(revent)
 
 def on_user_change_nick(revent):
     for function in dispatch['user_change_nick']:
-    	function(revent)
+        function(revent)
