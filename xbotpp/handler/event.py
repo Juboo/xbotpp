7364dc49141e7ae1c04defa38a3946e27b49c3a4
Zarek Jenkinson <aki@aki.pw>
2013-06-10 23:35:08 +1200

    spaces

--- xbotpp/handler/event.py
+++ xbotpp/handler/event.py
@@ -10,14 +10,14 @@ class message(generic):
         self.message = message
 
 class user_join(generic):
-	def __init__(self, user):
-		self.user = user
+    def __init__(self, user):
+        self.user = user
 
 class user_part(generic):
-	def __init__(self, user):
-		self.user = user
+    def __init__(self, user):
+        self.user = user
 
 class user_change_nick(generic):
-	def __init__(self, before, after):
-		self.before = before
-		self.after = after
+    def __init__(self, before, after):
+        self.before = before
+        self.after = after
