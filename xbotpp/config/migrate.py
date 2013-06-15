2f86f7ea45d6d560a8c7494f98af0eae5251467d
Zarek Jenkinson <aki@aki.pw>
2013-06-15 18:37:23 +1200

    added docs to migrate

--- xbotpp/config/migrate.py
+++ xbotpp/config/migrate.py
@@ -10,6 +10,10 @@ from configparser import ConfigParser
 '''Utility to migrate a pre-v0.3.x xbot++ config to the new format.'''
 
 def parse_args(args=None):
+    '''\
+    Parse the command-line arguments for :py:func:`xbotpp.config.migrate.main`.
+    '''
+    
     parser = argparse.ArgumentParser(description='Utility to migrate a pre-v0.3.x xbot++ config to the new format.')
     parser.add_argument('old_config', metavar='INPUT', 
         help='pre-v0.3.x config file to migrate')
@@ -19,6 +23,31 @@ def parse_args(args=None):
     return parser.parse_args(args)
 
 def main(options=None):
+    '''\
+    Start the migration utility.
+    
+    `options` is a object with the following attributes:
+
+    * `file`: the name of the file to write the migrated config to (or ``-`` for stdout)
+    * `old_config`: the name of the old config file to read and convert.
+
+    This object can be created by the :py:func:`xbotpp.config.migrate.parse_args` function,
+    which uses ArgumentParser, or by doing something like the following:
+
+        >>> class Object: pass
+        >>> options = Object()
+        >>> options.file = 'config.json'
+        >>> options.old_config = 'xbotpp.conf'
+        >>> xbotpp.config.migrate.main(options)
+
+    This function will return True on success, or raise a SystemExit exception with a 
+    numerical error code on an error, which are listed below:
+
+    * 1: Failed while reading the old configuration
+    * 2: Failed while converting the configuration
+    * 255: Failed while writing the configuration
+    '''
+
     options = options or parse_args()
 
     oldconf = ConfigParser()
@@ -39,26 +68,32 @@ def main(options=None):
         print("failed :(", file=sys.stderr)
         raise SystemExit(1)
 
-    for s in oldconf.sections():
-        print(" - %s" % s, file=sys.stderr)
-        if s.startswith('network: '):
-            y = re.sub('network: ', '', s)
-            newconf['networks'][y] = {'protocol': 'irc'}
-            for key in oldconf[s]:
-                if key == 'channels' or key == 'hosts':
-                    newconf['networks'][y][key] = oldconf[s][key].split(',')
-                else:
-                    newconf['networks'][y][key] = oldconf[s][key]
-        elif s.startswith('module: '):
-            y = re.sub('module: ', '', s)
-            newconf['modules']['load'].append(y)
-            newconf['modules'][y] = {}
-            for key in oldconf[s]:
-                newconf['modules'][y][key] = oldconf[s][key]
-        else:
-            newconf[s] = {}
-            for key in oldconf[s]:
-                newconf[s][key] = oldconf[s][key]
+    print("Starting conversion... ", file=sys.stderr)
+    try:
+        for s in oldconf.sections():
+            print(" - %s" % s, file=sys.stderr)
+            if s.startswith('network: '):
+                y = re.sub('network: ', '', s)
+                newconf['networks'][y] = {'protocol': 'irc'}
+                for key in oldconf[s]:
+                    if key == 'channels' or key == 'hosts':
+                        newconf['networks'][y][key] = oldconf[s][key].split(',')
+                    else:
+                        newconf['networks'][y][key] = oldconf[s][key]
+            elif s.startswith('module: '):
+                y = re.sub('module: ', '', s)
+                newconf['modules']['load'].append(y)
+                newconf['modules'][y] = {}
+                for key in oldconf[s]:
+                    newconf['modules'][y][key] = oldconf[s][key]
+            else:
+                newconf[s] = {}
+                for key in oldconf[s]:
+                    newconf[s][key] = oldconf[s][key]
+    except:
+        print("failed :(", file=sys.stderr)
+        print(e, file=sys.stderr)
+        raise SystemExit(2)
 
     sys.stderr.write("Adding module paths... ")
 
@@ -81,11 +116,12 @@ def main(options=None):
 
         json.dump(newconf, fh, indent=4, separators=(',', ': '), sort_keys=True)
         print("done.", file=sys.stderr)
+        return True
 
     except Exception as e:
         print("failed :(", file=sys.stderr)
         print(e, file=sys.stderr)
-        raise SystemExit(3)
+        raise SystemExit(255)
 
 if __name__ == "__main__":
     main()
