78217ddf2012f1e36e7381abbede6497cebe3177
Zarek Jenkinson <aki@aki.pw>
2013-06-16 12:15:58 +1200

    heh.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # vim: noai:ts=4:sw=4:expandtab:syntax=python
 
-import xbotpp
+import pkg_resources
 from setuptools import setup
 
 
 setup(
     name='xbotpp',
-    version=xbotpp.__version__,
+    version=pkg_resources.get_distribution('xbotpp').version,
     description='A multi-protocol chat bot.',
     author='aki--aki',
     author_email='aki@aki.pw',
