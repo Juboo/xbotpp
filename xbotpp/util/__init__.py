afc4949598130c0e5284d224680be09d93bf47db
Aki Jenkinson <aki@aki.pw>
2013-09-23 23:05:18 +1200

    [util] Added helper functions to util

--- xbotpp/util/__init__.py
+++ xbotpp/util/__init__.py
@@ -40,3 +40,68 @@ class CaseInsensitiveObjectDict(CaseInsensitiveDict):
 
 	def __setattr__(self, key, value):
 		super().__setitem__(key, value)
+
+def time_metric(secs=60):
+	"""Returns user-readable string representing given number of seconds."""
+	time = ''
+	for metric_secs, metric_char in [[7*24*60*60, 'w'], [24*60*60, 'd'], [60*60, 'h'], [60, 'm']]:
+		if secs > metric_secs:
+			time += '{}{}'.format(int(secs / metric_secs), metric_char)
+			secs -= int(secs / metric_secs) * metric_secs
+	if secs > 0:
+		time += '{}s'.format(secs)
+	return time
+
+def metric(num):
+	"""Returns user-readable string representing given number."""
+	for metric_raise, metric_char in [[9, 'B'], [6, 'M'], [3, 'k']]:
+		if num > (10 ** metric_raise):
+			return '{:.1f}{}'.format((num / (10 ** metric_raise)), metric_char)
+	return str(num)
+
+def pretty_date(time=False):
+	"""
+	Get a datetime object or a int() Epoch timestamp and return a
+	pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+	'just now', etc
+	"""
+
+	from datetime import datetime
+	now = datetime.now()
+	if type(time) is int:
+		diff = now - datetime.fromtimestamp(time)
+	elif isinstance(time,datetime):
+		diff = now - time 
+	elif not time:
+		diff = now - now
+	second_diff = diff.seconds
+	day_diff = diff.days
+
+	if day_diff < 0:
+		return ''
+
+	if day_diff == 0:
+		if second_diff < 10:
+			return "just now"
+		if second_diff < 60:
+			return str(int(second_diff)) + " seconds ago"
+		if second_diff < 120:
+			return  "a minute ago"
+		if second_diff < 3600:
+			return str(int(second_diff / 60)) + " minutes ago"
+		if second_diff < 7200:
+			return "an hour ago"
+		if second_diff < 86400:
+			return str(int(second_diff / 3600)) + " hours ago"
+	if day_diff == 1:
+		return "Yesterday"
+	if day_diff < 7:
+		return str(int(day_diff)) + " days ago"
+	if day_diff < 31:
+		return str(int(day_diff/7)) + " weeks ago"
+	if day_diff < 365:
+		return str(int(day_diff/30)) + " months ago"
+	return str(int(day_diff/365)) + " years ago"
+
+def smart_truncate(content, length=100, suffix='...'):
+	return (content if len(content) <= length else content[:length].rsplit(' ', 1)[0]+suffix)
